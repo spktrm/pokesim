7e5377e2a4636fdbd0905fc862059b44f197d236
spktrm <joseph.twin14@gmail.com>
2024-02-01 21:25:52 +1000

    sep python and js

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -10,19 +10,23 @@ PROTOC_GEN_TS_PATH="${ROOT_DIR}/node_modules/.bin/protoc-gen-ts"
 SRC_DIR="${ROOT_DIR}/src/proto"
 
 # Directory to write generated code (.d.ts files)
-OUT_DIR="${ROOT_DIR}/src/generated"
+PY_OUT_DIR="${ROOT_DIR}/pokesim/generated"
+JS_OUT_DIR="${ROOT_DIR}/src/generated"
 
 # Clean all existing generated files
-rm -r "${OUT_DIR}"
-mkdir "${OUT_DIR}"
+rm -r "${PY_OUT_DIR}"
+mkdir "${PY_OUT_DIR}"
+
+rm -r "${JS_OUT_DIR}"
+mkdir "${JS_OUT_DIR}"
 
 # Generate all messages
 protoc \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --ts_opt=esModuleInterop=true \
-    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
-    --ts_out="${OUT_DIR}" \
+    --js_out="import_style=commonjs,binary:${JS_OUT_DIR}" \
+    --ts_out="${JS_OUT_DIR}" \
     --proto_path="${SRC_DIR}" \
-    --python_out="${OUT_DIR}" \
+    --python_out="${PY_OUT_DIR}" \
     $(find "${SRC_DIR}" -iname "*.proto")
 
